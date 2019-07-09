dd942d8e65b39ce520a7f70b77d2c807f1d87456
garawalid <gwalid94@gmail.com>
2019-07-09 10:38:08 +0200

    Add set of distributed methods

---
+++ combine/layers/batch/utils_distributed.py
@@ -0,0 +1,6 @@
+
+def read_from_hdfs(table_name=""):
+    raise NotImplementedError('read_from_hdfs is not implemented yet!')
+
+def read_from_s3(table_name=""):
+    raise NotImplementedError('read_from_s3 is not implemented yet')
