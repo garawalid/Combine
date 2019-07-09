d5341c7c8da8e351e57fd4be4973b4c1035c1af9
garawalid <gwalid94@gmail.com>
2019-07-10 00:45:21 +0200

    Add Pipeline layout

---
+++ combine/pipelines/pipeline.py
@@ -0,0 +1,36 @@
+class Pipeline:
+
+    def __init__(self, id, name, data_source, data_destination, responsible, table_name=None):
+        # Abstract class of Pipeline
+        self.id = id
+        self.name = name
+        self.data_source = data_source
+        self.data_destination = data_destination
+        self.responsible = responsible
+        if table_name is None:
+            raise ValueError('We cannot initiate the pipeline. Please pass the datalake_path.')
+        else:
+            self.table_name = table_name
+
+    def run(self):
+        # launch pipeline
+        pass
+
+    def _preprocess(self):
+        # check the quality of the data
+        # Compute stats
+        pass
+
+    def _connect(self):
+        pass
+
+    def _process(self):
+        # Import transformations either from batch or streaming layer
+        pass
+
+    def _postprocess(self):
+        pass
+
+    def _save(self):
+        # save output into a datawarehouse
+        pass
