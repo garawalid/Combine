cc64a8bd98caf6e1a245a772afd1f9760d6bf11b
garawalid <gwalid94@gmail.com>
2019-07-09 23:53:30 +0200

    Update batch layer

--- combine/layers/batch/utils_distributed.py
+++ combine/layers/batch/utils_distributed.py
@@ -1,6 +1,9 @@
-def read_from_hdfs(table_name=""):
+# Some useful distributed methods
+
+
+def read_from_hdfs(table_name=None):
     raise NotImplementedError('read_from_hdfs is not implemented yet!')
 
 
-def read_from_s3(table_name=""):
+def read_from_s3(table_name=None):
     raise NotImplementedError('read_from_s3 is not implemented yet')
